cdee3023dc4628e8ba6c1b7a9aea078c2ba2915e
liyuho <liviahori@gmail.com>
2026-09-12 16:16:32 -0300

    implementa adição de tarefas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tarefa, setTarefa] = useState('');
+  const [tarefas, setTarefas] = useState([]);
+
+  function adicionarTarefa() {
+    if (tarefa.trim() === '') {
+      return;
+    }
+
+    setTarefas([...tarefas, tarefa]);
+    setTarefa('');
+  }
+
   return (
     <div className="app">
       <h1>Minha Lista de Tarefas</h1>
@@ -9,10 +22,20 @@ function App() {
         <input
           type="text"
           placeholder="Digite uma tarefa..."
+          value={tarefa}
+          onChange={(e) => setTarefa(e.target.value)}
         />
 
-        <button>Adicionar</button>
+        <button onClick={adicionarTarefa}>
+          Adicionar
+        </button>
       </div>
+
+      <ul>
+        {tarefas.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
