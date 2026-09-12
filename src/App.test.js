fbecfcbf56cf3b84ce2ef492969457e76397204b
liyuho <liviahori@gmail.com>
2026-09-12 16:57:11 -0300

    atualiza testes e dependências

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders task list title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const titleElement = screen.getByText(/minha lista de tarefas/i);
+
+  expect(titleElement).toBeInTheDocument();
 });
